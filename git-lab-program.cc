ff262040ea404fe6659581226466f4329bf8bb4b
Sam Slyder <ss397218@ohio.edu>
2019-09-04 17:26:39 -0400

    Adding the git-lab-program.cc

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Sam Slyder>
+ *        Date: <9/4/19>
+ * Description: Aworking with Git
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /*add code*/
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
